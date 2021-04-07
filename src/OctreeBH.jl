427eb39ba753be17df1f5d91b97a5a96994119a6
Chia-Yu Hu <cyhu.astro@gmail.com>
2021-04-07 21:47:36 +0200

    export isLeaf & AbstractData

--- src/OctreeBH.jl
+++ src/OctreeBH.jl
@@ -1,10 +1,11 @@
 module OctreeBH
 using StaticArrays
 
-export Node, Data, GravTreeGather
+export Node, Data, AbstractData, GravTreeGather
 export buildtree, get_scatter_ngb_tree, get_gather_ngb_tree
 export gravity_treewalk!, gravity_bruteforce!
 export nearest
+export isLeaf
 
 include("particle_data.jl");
 include("tree.jl");
